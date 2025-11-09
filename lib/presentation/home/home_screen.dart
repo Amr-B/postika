6adee36c106aa516a3f77542d59a2f70f4cdb1ca
Amr <amr.b4dev@gmail.com>
2025-11-09 17:06:10 +0200

    [UI] added products gridView to home

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:poskita/common/home_app_bar.dart';
+import 'package:poskita/constants/strings/images.dart';
 import 'package:poskita/constants/strings/text.dart';
 import 'package:poskita/constants/theme/colors.dart';
 import 'package:poskita/constants/theme/sizes.dart';
 import 'package:poskita/presentation/home/temp/temp_product_containers.dart';
 import 'package:poskita/presentation/home/widgets/home_search_bar.dart';
 import 'package:poskita/presentation/home/widgets/in_order_list.dart';
+import 'package:poskita/presentation/home/widgets/product_card.dart';
 import 'package:poskita/presentation/home/widgets/sales_container.dart';
 import 'package:poskita/presentation/home/widgets/total_status.dart';
 import 'package:poskita/presentation/home/widgets/transaction_widgets.dart';
@@ -21,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: AppColors.appBackground,
       appBar: HomeAppBar(),
@@ -63,7 +66,54 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
 
               // > products in low stock
-              TempProductContainers()
+              TempProductContainers(),
+              SizedBox(height: AppSizes.md),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'All Products',
+                    style: TextStyle(
+                      fontSize: AppSizes.font18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'See All',
+                      style: TextStyle(
+                        fontSize: AppSizes.font16,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 4,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 12,
+                  childAspectRatio: screenWidth < 600
+                      ? AppSizes.m50 // 0.60 for phones
+                      : AppSizes.m70, // 0.70 for tablets
+                ),
+                itemBuilder: (context, index) {
+                  return const ProductCard(
+                    title: AppTexts.bananaPancake,
+                    subTitle: AppTexts.bpSubT,
+                    image: AppImages.pancake,
+                    price: '6.00',
+                  );
+                },
+              )
             ],
           ),
         ),
---
+++ lib/presentation/home/widgets/product_card.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.subTitle,
+    required this.image,
+    required this.price,
+  });
+
+  final String title;
+  final String subTitle;
+  final String image;
+  final String price;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = constraints.maxWidth;
+        final imageHeight = width; // make image square
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // image
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    image,
+                    width: double.infinity,
+                    height: imageHeight,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  subTitle,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$ $price',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF0C3D91),
+                      ),
+                    ),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0C3D91),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.white,
+                          size: 22,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
