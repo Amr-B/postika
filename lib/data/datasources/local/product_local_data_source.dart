f681ff383ccbef8585b67e7697a3d4bbbd808b4f
Amr <amr.b4dev@gmail.com>
2025-11-15 16:51:07 +0200

    [Data] added products database

--- lib/data/datasources/local/product_local_data_source.dart
+++ lib/data/datasources/local/product_local_data_source.dart
@@ -1,30 +1,38 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:poskita/utils/helpers/database_helper.dart';
+
 import '../../models/product_model.dart';
+import 'product_db.dart';
 
 class ProductLocalDataSource {
-  final dbHelper = DatabaseHelper.instance;
-
-  Future<List<ProductModel>> getAllProducts() async {
-    final db = await dbHelper.database;
-    final result = await db.query('products');
-    return result.map((e) => ProductModel.fromMap(e)).toList();
-  }
-
-  Future<void> insertProduct(ProductModel product) async {
-    final db = await dbHelper.database;
-    await db.insert('products', product.toMap(),
+  Future<int> insertProduct(ProductModel product) async {
+    final db = await ProductDB.instance.database;
+    return await db.insert('products', product.toMap(),
         conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
-  Future<void> updateProduct(ProductModel product) async {
-    final db = await dbHelper.database;
-    await db.update('products', product.toMap(),
-        where: 'id = ?', whereArgs: [product.id]);
+  Future<int> updateProduct(ProductModel product) async {
+    final db = await ProductDB.instance.database;
+    return await db.update(
+      'products',
+      product.toMap(),
+      where: 'id = ?',
+      whereArgs: [product.id],
+    );
   }
 
-  Future<void> deleteProduct(int id) async {
-    final db = await dbHelper.database;
-    await db.delete('products', where: 'id = ?', whereArgs: [id]);
+  Future<int> deleteProduct(int id) async {
+    final db = await ProductDB.instance.database;
+    return await db.delete(
+      'products',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<List<ProductModel>> getAllProducts() async {
+    final db = await ProductDB.instance.database;
+    final rows = await db.query('products');
+
+    return rows.map((e) => ProductModel.fromMap(e)).toList();
   }
 }
