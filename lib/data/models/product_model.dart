cbaf021213846fe27bd052845f5b1a699cd67283
Amr <amr.b4dev@gmail.com>
2025-11-15 15:30:13 +0200

    [Data] added product model

---
+++ lib/data/models/product_model.dart
@@ -0,0 +1,36 @@
+import 'package:poskita/domain/entities/product.dart';
+
+class ProductModel extends Product {
+  ProductModel({
+    required int id,
+    required String name,
+    required String description,
+    required double price,
+    int quantity = 0,
+  }) : super(
+          id: id,
+          name: name,
+          description: description,
+          price: price,
+          quantity: quantity,
+        );
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      price: map['price'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'quantity': quantity,
+    };
+  }
+}
